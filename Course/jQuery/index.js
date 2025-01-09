a6ce0cd849a5b63c9e3570a2786aa8e1bfbf624b
Brandon Haupt <BrandonLHaupt@gmail.com>
2025-01-09 00:24:36 -0600

    Learning jQuery

--- Course/jQuery/index.js
+++ Course/jQuery/index.js
@@ -1,3 +1,18 @@
-$(document).ready(function () {
-  $("h1").css("color", "red");
-});
+// ! This allows the HTML to load before it runs the code inside of the ready
+// $(document).ready(function () {
+//   $("h1").css("color", "red");
+// });
+
+//* This shows the rgb value in the console
+//! console.log($("h1").css("color"));
+//* This shows the font-size value in the console
+//! console.log($("h1").css("font-size"));
+
+//* document.querySelectorAll("button")
+//! this does exactly the same thing as the above
+$("button");
+
+
+//! can add mutliple classes by adding a space after each class name
+$("h1").addClass("big-title margin-50")
+// $("h1").removeClass("big-title")
