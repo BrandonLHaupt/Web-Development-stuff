0697a599631f05d5bf6498289d61da7bcd8ec0ac
Brandon Haupt <BrandonLHaupt@gmail.com>
2025-01-11 13:42:57 -0600

    Update Section 23 - 2.4 QR Code Project
    - finished the QR code project

--- Course/Section 23/2.3 NPM/index.js
+++ Course/Section 23/2.3 NPM/index.js
@@ -1,4 +1,11 @@
-let generateName = require("sillyname");
-let sillyName = generateName();
+// let generateName = require("sillyname");
+// let sillyName = generateName();
 
-console.log(`My name is ${sillyName}.`);
+// console.log(`My name is ${sillyName}.`);
+
+import { randomSuperhero } from "superheroes";
+// Doesnt work (lines 8 and 9)
+// import superheroes from "superheroes";
+// const name = superheroes.random();
+
+console.log(`I am ${randomSuperhero()}!`);
