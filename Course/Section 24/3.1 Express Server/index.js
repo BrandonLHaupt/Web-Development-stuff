b365ede4e5e788235f8560b05f5c00abd2215ee6
Brandon Haupt <BrandonLHaupt@gmail.com>
2025-01-14 15:59:05 -0600

    Added course files for Section 24

--- Course/Section 24/3.1 Express Server/index.js
+++ Course/Section 24/3.1 Express Server/index.js
@@ -2,6 +2,13 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+  res.send(`Hello World!`);
+});
+
+//  () => is called anonymouys function
+// in ES6 it is also called an arrow function
 app.listen(port, () => {
-  console.log(`Server running on port ${port}.`);
+  // Call back function that is triggered when the server is setup and will trigger this function
+  console.log(`server is running on port ${port}`);
 });
--- Course/Section 24/3.2 HTTP Requests/index.js
+++ Course/Section 24/3.2 HTTP Requests/index.js
@@ -1,19 +1,28 @@
-import express from "express";
-const app = express();
-const port = 3000;
+import express from 'express'
+const app = express()
+const port = 3000
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello</h1>");
-});
+// app.get('/', (req, res) => {
+//   res.(`/home`)
+// })
 
-app.get("/about", (req, res) => {
-  res.send("<h1>About Me</h1><p>My name is Angela</p>");
-});
+app.get('/home', (req, res) => {
+  res.send(`<h1>Welcome to the home page!</h1>`)
+})
 
-app.get("/contact", (req, res) => {
-  res.send("<h1>Contact Me</h1><p>Phone: +44123456789</p>");
-});
+app.get('/', (req, res) => {
+  res.send(`<h1>Welcome to the home page!</h1>`)
+})
 
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
+app.get('/about', (req, res) => {
+  res.send(`<h1>This is the About page!</h1>`)
+})
+
+app.get('/contact', (req, res) => {
+  res.send(`<h1>Welcome to the Contact page!</h1>`)
+})
+
+
+app.listen(port, ()=>{
+  console.log(`listening on port ${port}`)
+})
